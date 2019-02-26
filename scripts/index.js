e62969173310559445369530f7693072029170a2
Zuzala <apmalinsky@gmail.com>
2019-02-25 22:02:44 -0500

    transition update

--- scripts/index.js
+++ scripts/index.js
@@ -5,21 +5,8 @@ $('a').click(function(e){
     $('#table').addClass('hidden');
     var direction = $(this).text();
 
-    switch(direction){
-        case 'Research':
-            transitionParticles(direction);
-            break;
-        case 'Creative':
-            transitionParticles(direction);
-            break;
-        case 'About':
-            transitionParticles(direction);
-            break;
-        case 'Projects':
-            transitionParticles(direction);
-            break;
- 
-    }
+    transitionParticles(direction);
+      
     setTimeout(function(){
         window.location.href = direction + '.html';
     }, 5000);
